cd9ec38d57ccdb9aa7e45bcad7e0b778f2590f1e
Ozancan Direk <ozan_direk@hotmail.com>
2025-04-24 23:37:44 +0300

    Add files via upload

---
+++ C#Projeleri/Algorithm/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter word and number (e.g. merhaba,3):");
+string input = Console.ReadLine();
+
+string[] parts = input.Split(",");
+
+
+string word = parts[0];
+int number = int.Parse(parts[1]);
+
+string dif = word.Substring(0, number);
+Console.WriteLine(dif);
