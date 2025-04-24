6f5f2a2540db1954f6028fc410bb43d292a464c6
Ozancan Direk <ozan_direk@hotmail.com>
2025-04-24 23:27:06 +0300

    Add files via upload

---
+++ C#Projeleri/Circle/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Yarıçapı giriniz: ");
+int r = Convert.ToInt32(Console.ReadLine());
+
+for (int y = -r; y <= r; y++)
+{
+    for (int x = -r; x <= r; x++)
+    {
+        double mesafe = Math.Sqrt(x * x + y * y);
+        if (mesafe > r - 0.5 && mesafe < r + 0.5)
+        {
+            Console.Write("*");
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+    }
+    Console.WriteLine();
+}
