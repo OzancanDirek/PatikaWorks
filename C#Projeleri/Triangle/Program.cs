1be2c399ab3f347d610efa08e03da176d1008a31
Ozancan Direk <ozan_direk@hotmail.com>
2025-04-24 23:22:36 +0300

    Add files via upload

---
+++ C#Projeleri/Triangle/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Please enter the size of triangle");
+int size = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= size; i++)
+{
+    for (int j = 1; j <= size - i; j++)
+    {
+        Console.Write(" ");
+    }
+
+    for (int k = 1; k <= 2 * i - 1; k++)
+    {
+        Console.Write("*");
+    }
+
+    Console.WriteLine(); 
+}
+    
+
+
