010d3ebe7878bf01050b472875fef42d16374c4a
Ozancan Direk <ozan_direk@hotmail.com>
2025-04-24 23:50:19 +0300

    Add files via upload

---
+++ C#Projeleri/ReverseSentences/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter your sentences:");
+string input = Console.ReadLine();
+
+string[] words = input.Split(" ");
+
+for (int i = 0; i < words.Length; i++)
+{
+    char[] chars = words[i].ToCharArray(); 
+    Array.Reverse(chars);
+    Console.WriteLine(chars);
+}
+Console.ReadLine();
