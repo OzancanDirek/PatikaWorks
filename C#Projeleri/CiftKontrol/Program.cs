99746501a54b6053ce31eade6f42c3c49c27e618
Ozancan Direk <ozan_direk@hotmail.com>
2025-04-25 19:27:53 +0300

    Add files via upload

---
+++ C#Projeleri/CiftKontrol/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Sayıları giriniz (örnek: 2 3 1 5 3 3):");
+        string input = Console.ReadLine();
+
+        string[] list = input.Split(' '); // bosluk bırakarak sayıları liste kayıt ediyorum
+
+        for (int i = 0; i < list.Length; i += 2)
+        {
+            int sayı1 = int.Parse(list[i]); 
+            int sayı2 = int.Parse(list[i + 1]); //ikinci sayıyı aldım
+
+            int toplam = sayı1 + sayı2;
+
+            if (sayı1 == sayı2)
+            {
+                Console.Write((toplam * toplam) + " "); //karesini almak için
+            }
+            else
+            {
+                Console.Write(toplam + " "); 
+            }
+        }
+
+        Console.WriteLine(); // Alt satıra geçmek için
+    }
+}
