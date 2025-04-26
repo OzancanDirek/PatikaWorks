61a11201dccf830705e670c8a92cb8e1b94b49e1
Ozancan Direk <ozan_direk@hotmail.com>
2025-04-26 19:12:39 +0300

    Add files via upload

---
+++ C#Projeleri/Constant/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+Console.WriteLine("Kontrol etmek istediğiniz kelimeyi lütfen yazınız");
+string input = Console.ReadLine();
+
+bool varmı = false;
+
+char[] sessizHarfler = { 'z', 'y', 'v', 't', 'ş', 's', 'r', 'p', 'n', 'm', 'l', 'k', 'h', 'j', 'ğ', 'g', 'd', 'ç', 'c', 'b' };
+
+for (int i = 0; i < input.Length - 1; i++)
+{
+    char ifade = input[i];
+    char ifadeyan = input[i + 1];
+
+    if (sessizHarfler.Contains(ifade) && sessizHarfler.Contains(ifadeyan))
+    {
+        varmı = true;
+        break;
+    }
+}
+
+if (varmı)
+{
+    Console.WriteLine($"Arka arkaya iki sessiz harf var: {varmı}");
+}
+else
+{
+    Console.WriteLine($"Arka arkaya iki sessiz harf yok: {varmı}");
+}
