ecacbdaed06949f637f94709b40beac4128be0cf
Ozancan Direk <ozan_direk@hotmail.com>
2025-04-27 20:55:25 +0300

    Add files via upload

---
+++ C#Projeleri/BarcodeGeneratorReader/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using ZXing;
+using ZXing.Windows.Compatibility;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Veri girin:");
+        string data = Console.ReadLine();
+
+        BarcodeWriter writer = new BarcodeWriter();
+        writer.Format = BarcodeFormat.QR_CODE;
+        writer.Options = new ZXing.Common.EncodingOptions
+        {
+            Width = 400,
+            Height = 400,
+            Margin = 0
+        };
+
+        var result = writer.Write(data);
+
+        string path = "barcode.png";
+        if (File.Exists(path))
+            File.Delete(path);
+
+        result.Save(path, System.Drawing.Imaging.ImageFormat.Png);
+        Console.WriteLine($"Barcode kaydedildi: {path}");
+
+        var reader = new BarcodeReader();
+        var barcodeBitmap = new Bitmap(path);
+        var decodeResult = reader.Decode(barcodeBitmap);
+
+        if (decodeResult != null)
+        {
+            Console.WriteLine($"Okunan Barkod Verisi: {decodeResult.Text}");
+        }
+        else
+        {
+            Console.WriteLine("Barkod okunamadı.");
+        }
+    }
+}
